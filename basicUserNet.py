35cd589246e401ddc66177001d460e0e618245a6
DPCollins <daniel.collins@digital.cabinet-office.gov.uk>
2014-08-15 09:46:36 +0100

    Create basicUserNet.py
    Script imports newt and allows specification of accounts and friend/follower type in the command line arguments.

---
+++ basicUserNet.py
@@ -0,0 +1,69 @@
+import sys,newt
+import mechanize
+# from dbconn import DropboxConnection
+import os
+
+def report(m):
+  newt.report(m,True)
+
+api=newt.getTwitterAPI()
+# print api.test()
+#----------------------------------------------------------------
+#user settings
+user=sys.argv[1]
+
+try:
+  typ=sys.argv[2]
+except:
+  typ="friends"
+  
+try:
+  extra=sys.argv[3]
+except:
+  extra=-1
+
+try:
+#   sampleSize=int(sys.argv[4])
+# 1400 24hr max due to dyno cycling
+  sampleSize='1350'
+except:
+  sampleSize='all'
+#----------------------------------------------------------------
+tw={}
+
+if typ=="followers":
+  tw=newt.getTwitterFollowersDetailsByIDs(api,user,sampleSize)
+else:
+  tw=newt.getTwitterFriendsDetailsByIDs(api,user,sampleSize)
+
+f=newt.openTimestampedFile(user+'/'+typ,'sample'+str(sampleSize)+'tweeps.txt')
+for tweep in tw:
+    f.write(str(tweep)+'\n')
+f.close()
+
+report("List members:")
+for i in tw:
+  report(tw[i].screen_name)
+  
+'''
+report("List members:")
+for i in tw:
+  report(tw[i].screen_name)
+'''  
+#newt.gephiOutputFile(api,user+'/'+typ, tw,'outerfriends')
+newt.gephiOutputFile(api,user+'/'+typ, tw)
+if extra!=-1:
+	newt.gephiOutputFile(api,user+'/'+typ, tw,'extrafriends')
+	#newt.gephiOutputFile(api,user+'/'+typ, tw,'outerfollowers')
+'''
+newt.gephiOutputFile(api,user, tw,'outerfriends')
+newt.gephiOutputFile(api,user, tw,'outerfollowers')
+newt.googleCSEDefinitionFile("XXX",user, tw)
+newt.googleCSEDefinitionFileWeighted("XXX",user, tw)
+'''
+
+# upload_file(local_file,remote_dir,remote_file,email,password)
+
+
+
+
